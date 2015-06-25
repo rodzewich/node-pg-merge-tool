6188a0114c7a4af50b50ae94f6f82d5f3339be45
rodzewich <rodzewich.oleg@gmail.com>
2015-06-25 12:04:13 +0300

    -

--- dump.sql
+++ dump.sql
@@ -1,8 +1,10 @@
 
+/* импортируемые таблицы */
 
 CREATE TABLE users (
   id INTEGER,
-  married BOOLEAN
+  married BOOLEAN,
+  CONSTRAINT "users_id_primary" PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
@@ -24,6 +26,22 @@ CREATE TABLE users2 (
 CREATE TABLE users3 (
   id INTEGER,
   married BOOLEAN
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE countries_dictionary (
+  id INTEGER,
+  name STRING,
+  CONSTRAINT "countries_dictionary_id_primary" PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE user_statuses_dictionary (
+  id INTEGER,
+  name STRING,
+  CONSTRAINT "user_statuses_dictionary_id_primary" PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
