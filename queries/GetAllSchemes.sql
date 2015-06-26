bc791b56d2ac2e681f5900f6c525826a38032292
rodzewich <rodzewich.oleg@gmail.com>
2015-06-26 11:02:22 +0300

    -

--- queries/GetAllSchemes.sql
+++ queries/GetAllSchemes.sql
@@ -14,8 +14,26 @@ SELECT n.nspname,
    AND d.classoid='pg_namespace'::regclass
  WHERE n.nspname NOT LIKE E'pg\\_%'
    AND NOT (
-       (n.nspname = 'pgagent' AND EXISTS (SELECT 1 FROM pg_class c1 WHERE c1.relname = 'pga_job' AND c1.relnamespace = n.oid LIMIT 1)) OR
-       (n.nspname = 'information_schema' AND EXISTS (SELECT 1 FROM pg_class c2 WHERE c2.relname = 'tables' AND c2.relnamespace = n.oid LIMIT 1)) OR
-       (n.nspname LIKE '_%' AND EXISTS (SELECT 1 FROM pg_proc p WHERE p.proname='slonyversion' AND p.pronamespace = n.oid LIMIT 1))
+           (n.nspname = 'pgagent' AND EXISTS (
+               SELECT 1
+                 FROM pg_class c1
+                WHERE c1.relname = 'pga_job'
+                  AND c1.relnamespace = n.oid
+                LIMIT 1
+           )) OR
+           (n.nspname = 'information_schema' AND EXISTS (
+               SELECT 1
+                 FROM pg_class c2
+                WHERE c2.relname = 'tables'
+                  AND c2.relnamespace = n.oid
+                LIMIT 1
+           )) OR
+           (n.nspname LIKE '_%' AND EXISTS (
+               SELECT 1
+                 FROM pg_proc p
+                WHERE p.proname='slonyversion'
+                  AND p.pronamespace = n.oid
+                LIMIT 1
+           ))
        )
  ORDER BY n.nspname;
