4cf6c4a5c94a6cc47bc3fa0cd4e960b18929cee6
rodzewich <rodzewich.oleg@gmail.com>
2015-06-26 19:35:18 +0300

    -

--- dump.sql
+++ dump.sql
@@ -1,10 +1,34 @@
 
-/* импортируемые таблицы */
+-- ftp://ftp.fu-berlin.de/doc/iso/iso3166-countrycodes.txt
+-- https://ru.wikipedia.org/wiki/ISO_3166-1
+-- https://en.wikipedia.org/?title=ISO_3166-1
+-- http://www.iso.org/iso/ru/home/standards/country_codes.htm
+CREATE TABLE countries_dictionary (
+  id INTEGER
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE states_dictionary (
+  id INTEGER,
+  country_id INTEGER
+) WITH (
+   OIDS=FALSE
+);
+
+CREATE TABLE cities_dictionary (
+  id INTEGER,
+  state_id INTEGER,
+  country_id INTEGER
+) WITH (
+   OIDS=FALSE
+);
 
 CREATE TABLE users (
-  id INTEGER,
-  married BOOLEAN,
-  CONSTRAINT "users_id_primary" PRIMARY KEY (id)
+  id       INTEGER,
+  login    CHARACTER VARYING(10),
+  password CHARACTER VARYING(10),
+  CONSTRAINT "users[id](primary)" PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
@@ -26,14 +50,6 @@ CREATE TABLE users2 (
 CREATE TABLE users3 (
   id INTEGER,
   married BOOLEAN
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE countries_dictionary (
-  id INTEGER,
-  name INTEGER,
-  CONSTRAINT "countries_dictionary_id_primary" PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
